1d17c827d0ef9213a4e4d97525b1e888eb65c59f
ahmedali1510 <152388611+ahmedali1510@users.noreply.github.com>
2026-09-05 10:17:22 +0300

    Update app.run to use environment variable for port

--- ahmed.py
+++ ahmed.py
@@ -854,4 +854,5 @@ with app.app_context():
     seed_data()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
